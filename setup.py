860dc1b94edeea9bdf9c6ee96e77fea974f6e6ba
Nathan <allen.nathanield@gmail.com>
2021-07-14 21:37:33 -0400

    location updates

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.rst","r") as fh:
 	long_description = fh.read()
 	
 setup(name='snipeitv2',
-      version='2.1',
+      version='2.1.1',
 	  long_description=long_description,
       long_description_content_type="text/markdown",
       description=("Python library to access the SnipeIT API"),
